49b7105238d58f4b3c60b0c06db6de28ce60cbd8
sparsh_thakur <113547853+skullemote@users.noreply.github.com>
2023-01-14 16:54:14 -0700

    Add Person Page
    Add person page has been updated with first name last name email and phone number

--- trip_app/lib/add_person.dart
+++ trip_app/lib/add_person.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trip_app/home_page.dart';
 
 class AddPersonPage extends StatelessWidget {
   const AddPersonPage({super.key});
@@ -9,9 +10,36 @@ class AddPersonPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add Person'),
       ),
-      body: const Center(
-        child: Text('Add Person'),
-      ),
+      body: Column(children: [
+        const Text('Enter your first name'),
+        const TextField(
+            decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'e.g. Sparsh',
+        )),
+        const Text('Enter your last name'),
+        const TextField(
+            decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'e.g. Thakur',
+        )),
+        const Text('Enter a valid Email id'),
+        const TextField(
+            decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'e.g. abc@gmail.com',
+        )),
+        const Text('Enter your phone number'),
+        const TextField(
+            decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'e.g. 123456789',
+        )),
+        ElevatedButton(
+            onPressed: (() => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const HomePage()))),
+            child: const Text('Submit')),
+      ]),
     );
   }
 }
