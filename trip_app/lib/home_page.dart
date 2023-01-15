3e90e4211ca999080692af5b5738fca6b7952564
Rajan Maghera <rmaghera@ualberta.ca>
2023-01-14 18:51:35 -0700

    Sample change

--- trip_app/lib/home_page.dart
+++ trip_app/lib/home_page.dart
@@ -7,8 +7,6 @@ import 'list_of_people.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  // This is a change
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
