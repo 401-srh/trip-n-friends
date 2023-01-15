bcaecfff9c6cf889d0209408baeb2cb627b1b258
Hyan320 <70993097+Hyan320@users.noreply.github.com>
2023-01-15 11:32:47 -0700

    Update list_of_people.dart

--- trip_app/lib/list_of_people.dart
+++ trip_app/lib/list_of_people.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:trip_app/controllers/data_controller.dart';
-
+import 'models/city.dart';
 import 'models/person.dart';
+
+City tokyo = City(cityName: "Tokyo", country: "Japan");
+City edm = City(cityName: "Edmonton", country: "Canada");
+City syd = City(cityName: "Sydney", country: "Australia");
+
+List<List<City>> liked_cities = [
+  [tokyo, syd],
+  [tokyo, edm]
+];
+
+List<Person> person_list = [
+  Person(firstName: "Han", lastName: "Yan"),
+  Person(firstName: "Rajan", lastName: "Maghera"),
+];
 
 class ListOfPeople extends StatelessWidget {
   const ListOfPeople({super.key});
@@ -52,15 +66,34 @@ class _BasicListState extends State<BasicList> {
               ? const CircularProgressIndicator()
               : Expanded(
                   child: ListView.builder(
-                    itemCount: _data!.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(
-                            '${_data![index].firstName!} ${_data![index].lastName!}'),
-                      );
-                    },
-                  ),
-                ),
+                  itemCount: person_list.length,
+                  itemBuilder: (context, index) {
+                    List<String> cityLists = [];
+                    for (var cityList in liked_cities) {
+                      String finalList = "";
+                      for (var city in cityList) {
+                        finalList += "${city.cityName}, ${city.country} | ";
+                      }
+                      cityLists.add(finalList);
+                    }
+
+                    return Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${person_list[index].firstName} ${person_list[index].lastName}",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text("Liked cities: | ${cityLists[index]}")
+                        ],
+                      ),
+                    ));
+                  },
+                )),
         ],
       ),
     );
